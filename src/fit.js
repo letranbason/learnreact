8b80fa1355adf404210926865d4a8268c9017062
letranbason <109140737+letranbason@users.noreply.github.com>
2023-05-01 15:18:02 +0700

    learn link file css and event

--- src/fit.js
+++ src/fit.js
@@ -1,20 +1,25 @@
 import React from "react";
+import './fit.css';
 
 class Fit extends React.Component{
 
     state = {
         name: 'Son',
-        age: '18',
+        age: '22',
         years: '2001'
     }
-
+    handleonclick = () =>{
+        alert('learn react')
+    }
 
     render(){
         return(
             <>
-                <div>hello world form {this.state.name}</div>
-                <div>hello age form {this.state.age}</div>
+                <div className="begin">hello world form {this.state.name}</div>
+                <div className="begin1">hello age form {this.state.age}</div>
                 <div>hello years form {this.state.years}</div>
+
+                <button className="button" onClick={()=> this.handleonclick()}> click</button>
             </>
             
         )
